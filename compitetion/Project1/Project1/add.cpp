75b6b4ff4961a36646cbab1c484e2a9fba79aef6
guo-junchenhh <3361520296@qq.com>
2026-04-16 16:34:52 +0800

    多态

--- compitetion/Project1/Project1/add.cpp
+++ compitetion/Project1/Project1/add.cpp
@@ -1,12 +1,39 @@
-#define _CRT_SECURE_NO_WARNINGS 1
+﻿#define _CRT_SECURE_NO_WARNINGS 1
 #include<iostream>
 using namespace std;
+class Person {
+public:
+	virtual void BuyTicket() { cout << "买票-全价" << endl; }
+private:
+	string _name;
+};
+class Student : public Person {
+public:
+	virtual void BuyTicket() { cout << "买票-打折" << endl; }
+private:
+	string _id;
+};
+class Soldier : public Person {
+public:
+	virtual void BuyTicket() { cout << "买票-优先" << endl; }
+private:
+	string _codename;
+};
+void Func(Person* ptr)
+{
+	// 这里可以看到虽然都是Person指针Ptr在调用BuyTicket
+	// 但是跟ptr没关系，而是由ptr指向的对象决定的。
+	ptr->BuyTicket();
+}
 int main()
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	double i = 1;
-	double& r = i;
-	cout << r;
+	// 其次多态不仅仅发生在派生类对象之间，多个派生类继承基类，重写虚函数后
+	// 多态也会发生在多个派生类之间。
+	Person ps;
+	Student st;
+	Soldier sr;
+	Func(&ps);
+	Func(&st);
+	Func(&sr);
 	return 0;
 }
